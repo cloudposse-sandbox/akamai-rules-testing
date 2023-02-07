d28b95bae9f47f2539adedf70e3d897737ce2d32
Matt Calhoun <matt@cloudposse.com>
2023-02-07 18:06:24 -0500

    add error docs

--- main.tf
+++ main.tf
@@ -14,9 +14,18 @@ provider "akamai" {
 
 variable "one_enabled" {
   type    = bool
-  default = false
+  default = true
 }
 
+## This example is straight out of the docs at https://registry.terraform.io/providers/akamai/akamai/latest/docs/data-sources/property_rules_template
+## and does not work. 
+##
+## The error is:
+## │ Error: template: main:1: unexpected "]" in template clause
+## │
+## │   with data.akamai_property_rules_template.one[0],
+## │   on main.tf line 22, in data "akamai_property_rules_template" "one":
+## │   22: data "akamai_property_rules_template" "one" {
 data "akamai_property_rules_template" "one" {
   count = var.one_enabled ? 1 : 0
 
@@ -42,6 +51,15 @@ variable "two_enabled" {
   default = true
 }
 
+## This example works as long as behaviors only includes one include. As soon as you include more than one include, 
+## the error below is thrown:
+##
+## │ Error: template: main:4: unexpected "," in template clause
+## │
+## │   with data.akamai_property_rules_template.two[0],
+## │   on main.tf line 47, in data "akamai_property_rules_template" "two":
+## │   47: data "akamai_property_rules_template" "two" {
+## │
 data "akamai_property_rules_template" "two" {
   count = var.two_enabled ? 1 : 0
 
